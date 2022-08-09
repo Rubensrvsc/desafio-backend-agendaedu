f1dd8db2ea1c59dc2c4192f9c508b400a5423623
rubensrvsc <rubensspfc100@gmail.com>
2022-08-09 20:50:32 -0300

    refactor: update queries in repository

--- app/domain/business/repository.rb
+++ app/domain/business/repository.rb
@@ -1,21 +1,29 @@
 module Business
     class Repository
         
-        def all_congresspeople
-            Congressperson.all
+        def all_congresspeople(page)
+            Congressperson.page(page).per(5)
         end
 
         def sum_expenses_congressperson(congressperson_id)
-            
+            Congressperson.find_by(id: congressperson_id).expenses.sum(:vlrLiquido)
         end
 
-        def list_expenses(congressperson_id)
+        def list_expenses(congressperson_id, page)
             Congressperson.find_by(id: congressperson_id).expenses.select(
                 :datEmissao,
                 :txtFornecedor,
                 :vlrLiquido,
                 :urlDocumento
-            )
+            ).page(page).per(5)
+        end
+
+        def get_ide_Cadastro(congressperson_id)
+            Congressperson.find_by(id: congressperson_id).ideCadastro
+        end
+
+        def get_max_expense(congressperson_id)
+            Congressperson.find_by(id: congressperson_id).expenses.maximum(:vlrLiquido)
         end
     end
 end
