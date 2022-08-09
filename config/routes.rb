e062596e5cfe2a1b04f678809ae8849e58949126
rubensrvsc <rubensspfc100@gmail.com>
2022-08-09 20:50:52 -0300

    routes: add a new route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'congress/index', to: 'congress#index'
   post 'congress/csv', to: 'congress#csv'
   get 'congress/list', to: 'congress#congress_list'
+  get 'details/:id', to: 'congress#congressperson_details'
   mount Sidekiq::Web => '/sidekiq'
+
+  root to: 'congress#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
