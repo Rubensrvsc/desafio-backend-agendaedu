fec1b362ac4b0ea9c2b6d23eb548e277324b1f72
rubensrvsc <rubensspfc100@gmail.com>
2022-08-10 19:56:27 -0300

    test: test for controller

--- spec/controllers/congress_controller_spec.rb
+++ spec/controllers/congress_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe CongressController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe "Some features" do
+
+    context 'test imported csv' do
+      let!(:congressperson) { create(:congressperson) }
+      let!(:expenses) { create_list(:expense, 3, congressperson_id: congressperson.id) }
+
+      it "csv  imported" do
+        path = Rails.root.join('spec/upload/ano-2021.csv')
+        post :csv, params: { csv: path }
+  
+        expect(flash["alert"]).to eq("Já foi importado um csv")
+      end
     end
   end
-
 end
