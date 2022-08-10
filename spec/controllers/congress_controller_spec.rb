e642280954a4779643b6ab9a26f0f963d3ff9961
rubensrvsc <rubensspfc100@gmail.com>
2022-08-10 20:16:05 -0300

    teste: update test

--- spec/controllers/congress_controller_spec.rb
+++ spec/controllers/congress_controller_spec.rb
@@ -3,16 +3,23 @@ require 'rails_helper'
 RSpec.describe CongressController, type: :controller do
 
   describe "Some features" do
-
     context 'test imported csv' do
       let!(:congressperson) { create(:congressperson) }
       let!(:expenses) { create_list(:expense, 3, congressperson_id: congressperson.id) }
 
-      it "csv  imported" do
+      it "csv imported" do
         path = Rails.root.join('spec/upload/ano-2021.csv')
-        post :csv, params: { csv: path }
+        post :upload_csv, params: { csv: path }
   
         expect(flash["alert"]).to eq("Já foi importado um csv")
+      end
+
+      it 'get list of congresspeople' do
+        
+      end
+
+      it 'get one congresspeople' do
+        
       end
     end
   end
