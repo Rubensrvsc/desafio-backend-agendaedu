5dbb8f7efbaab6c76d09b2573bf795df3465a83d
rubensrvsc <rubensspfc100@gmail.com>
2022-08-11 11:33:10 -0300

    test: create directory if not exists

--- spec/domain/business/register_values_spec.rb
+++ spec/domain/business/register_values_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Business::RegisterValues do
         let!(:path) { Rails.root.join('spec/upload/ano-2021.csv') }
         let!(:ext) { File.extname(path) }
         let!(:file_name) { "#{SecureRandom.urlsafe_base64}#{ext}" }
+        let!(:new_dir) { FileUtils.mkdir_p(Rails.root.join("spec/upload/file/")) }
         let!(:file_name_path) { Rails.root.join("spec/upload/file/", file_name) }
         let!(:write_csv) do
             File.open(file_name_path, "wb") { |f| f.write(path.read) }
